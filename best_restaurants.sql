ad6bb1d7b2f64b83289fc22015c623822658cd77
James Turello <jdturello@gmail.com>
2016-02-25 06:24:05 -0800

    add updated sql dump file to project

--- best_restaurants.sql
+++ best_restaurants.sql
@@ -2,12 +2,16 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.5.0
+-- Dumped by pg_dump version 9.5.0
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
@@ -30,7 +34,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: cuisine; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: cuisine; Type: TABLE; Schema: public; Owner: jturello
 --
 
 CREATE TABLE cuisine (
@@ -39,10 +43,10 @@ CREATE TABLE cuisine (
 );
 
 
-ALTER TABLE cuisine OWNER TO "Guest";
+ALTER TABLE cuisine OWNER TO jturello;
 
 --
--- Name: cuisine_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: cuisine_id_seq; Type: SEQUENCE; Schema: public; Owner: jturello
 --
 
 CREATE SEQUENCE cuisine_id_seq
@@ -53,29 +57,30 @@ CREATE SEQUENCE cuisine_id_seq
     CACHE 1;
 
 
-ALTER TABLE cuisine_id_seq OWNER TO "Guest";
+ALTER TABLE cuisine_id_seq OWNER TO jturello;
 
 --
--- Name: cuisine_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: cuisine_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jturello
 --
 
 ALTER SEQUENCE cuisine_id_seq OWNED BY cuisine.cuisine_id;
 
 
 --
--- Name: restaurants; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: restaurants; Type: TABLE; Schema: public; Owner: jturello
 --
 
 CREATE TABLE restaurants (
     id integer NOT NULL,
-    name character varying
+    name character varying,
+    cuisine_id integer
 );
 
 
-ALTER TABLE restaurants OWNER TO "Guest";
+ALTER TABLE restaurants OWNER TO jturello;
 
 --
--- Name: restaurants_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: restaurants_id_seq; Type: SEQUENCE; Schema: public; Owner: jturello
 --
 
 CREATE SEQUENCE restaurants_id_seq
@@ -86,65 +91,73 @@ CREATE SEQUENCE restaurants_id_seq
     CACHE 1;
 
 
-ALTER TABLE restaurants_id_seq OWNER TO "Guest";
+ALTER TABLE restaurants_id_seq OWNER TO jturello;
 
 --
--- Name: restaurants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: restaurants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jturello
 --
 
 ALTER SEQUENCE restaurants_id_seq OWNED BY restaurants.id;
 
 
 --
--- Name: cuisine_id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: cuisine_id; Type: DEFAULT; Schema: public; Owner: jturello
 --
 
 ALTER TABLE ONLY cuisine ALTER COLUMN cuisine_id SET DEFAULT nextval('cuisine_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: id; Type: DEFAULT; Schema: public; Owner: jturello
 --
 
 ALTER TABLE ONLY restaurants ALTER COLUMN id SET DEFAULT nextval('restaurants_id_seq'::regclass);
 
 
 --
--- Data for Name: cuisine; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: cuisine; Type: TABLE DATA; Schema: public; Owner: jturello
 --
 
 COPY cuisine (cuisine_id, type) FROM stdin;
 1	American
 2	Southern
+3	Vietnamese
+4	Thai
+5	Italian
+6	Chinese
+7	Japanese/Sushi
+8	Indian
+9	Southern
+10	Mexican
 \.
 
 
 --
--- Name: cuisine_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: cuisine_id_seq; Type: SEQUENCE SET; Schema: public; Owner: jturello
 --
 
-SELECT pg_catalog.setval('cuisine_id_seq', 2, true);
+SELECT pg_catalog.setval('cuisine_id_seq', 10, true);
 
 
 --
--- Data for Name: restaurants; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: restaurants; Type: TABLE DATA; Schema: public; Owner: jturello
 --
 
-COPY restaurants (id, name) FROM stdin;
-1	Screen Door
-2	Lardo
+COPY restaurants (id, name, cuisine_id) FROM stdin;
+1	Screen Door	9
+2	Lardo	3
 \.
 
 
 --
--- Name: restaurants_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: restaurants_id_seq; Type: SEQUENCE SET; Schema: public; Owner: jturello
 --
 
 SELECT pg_catalog.setval('restaurants_id_seq', 2, true);
 
 
 --
--- Name: cuisine_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: cuisine_pkey; Type: CONSTRAINT; Schema: public; Owner: jturello
 --
 
 ALTER TABLE ONLY cuisine
@@ -152,7 +165,7 @@ ALTER TABLE ONLY cuisine
 
 
 --
--- Name: restaurants_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: restaurants_pkey; Type: CONSTRAINT; Schema: public; Owner: jturello
 --
 
 ALTER TABLE ONLY restaurants
@@ -160,12 +173,12 @@ ALTER TABLE ONLY restaurants
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: epicodus
+-- Name: public; Type: ACL; Schema: -; Owner: jturello
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM epicodus;
-GRANT ALL ON SCHEMA public TO epicodus;
+REVOKE ALL ON SCHEMA public FROM jturello;
+GRANT ALL ON SCHEMA public TO jturello;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
